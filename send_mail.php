3a091c720cc4462ef07176c3fdec0d3f79a4af3e
Andii-12 <60426649+Andii-12@users.noreply.github.com>
2024-02-14 01:11:30 +0800

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -4,9 +4,9 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
